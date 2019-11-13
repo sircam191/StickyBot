f4537d895246e381dddc6aff61bc6929ffa5284b
sircam191 <sircam19@gmail.com>
2019-11-12 20:13:34 -0800

    Update NewSticky.java

--- src/main/java/NewSticky.java
+++ src/main/java/NewSticky.java
@@ -11,7 +11,7 @@ public class NewSticky extends ListenerAdapter {
 
     Map<String, String> mapChannel = new HashMap<>();
     Map<String, String> mapMessage = new HashMap<>();
-
+    Map<String, String> mapDeleteId = new HashMap<>();
 
     public void onGuildMessageReceived(GuildMessageReceivedEvent event) {
         String[] args = event.getMessage().getContentRaw().split("\\s+");
@@ -28,6 +28,7 @@ public class NewSticky extends ListenerAdapter {
                     //Puts channel ID and message into maps
                     mapChannel.put(event.getGuild().getId(), event.getChannel().getId());
                     mapMessage.put(event.getGuild().getId(), "__**Pinned Message:**__\n\n" + event.getMessage().getContentRaw().substring(7));
+
 
                 } catch (Exception e) {
                 }
@@ -49,32 +50,26 @@ public class NewSticky extends ListenerAdapter {
             } else {
                 event.getMessage().addReaction("\u2705").queue();
             }
-           if (mapMessage.get(event.getGuild().getId()) != null) {
-               event.getChannel().getHistory().retrievePast(2).queue(m -> {
-                  m.get(1).delete().queue();
-               });
-           }
+            if(mapMessage.get(event.getGuild().getId()) != null) {
+                event.getChannel().deleteMessageById(mapDeleteId.get(event.getGuild().getId())).queue();
+            }
 
             mapMessage.put(event.getGuild().getId(), null);
             mapChannel.put(event.getGuild().getId(), null);
+            mapDeleteId.put(event.getGuild().getId(), null);
         }
 
         //Posts sticky message if mapMessage for the guild is not null and message is not from the sticky bot and channel is the same as the one used to start sticky command
         if (mapMessage.get(event.getGuild().getId()) != null && !event.getAuthor().getId().equals(Main.botId) && event.getChannel().getId().equals(mapChannel.get(event.getGuild().getId()))) {
             TextChannel textChannel = event.getGuild().getTextChannelById(mapChannel.get(event.getGuild().getId()));
-            textChannel.sendMessage(mapMessage.get(event.getGuild().getId())).queue();
+            textChannel.sendMessage(mapMessage.get(event.getGuild().getId())).queue(m -> mapDeleteId.put(event.getGuild().getId(), m.getId()));
         }
 
-        //Checks last 3 massages every time one is sent to check if it contains the sticky message, if so deletes it.
+        //Deletes old sticky message by message ID
         if(mapMessage.get(event.getGuild().getId()) != null) {
-            event.getChannel().getHistory().retrievePast(3).queue(m -> {
-                for (int i = 1; i < 3; i++) {
-                    if (m.get(i).getContentDisplay().contains(mapMessage.get(event.getGuild().getId()))) {
-                        m.get(i).delete().queue();
-                    }
-                }
-            });
+            event.getChannel().deleteMessageById(mapDeleteId.get(event.getGuild().getId())).queue();
         }
+
     }
 
     public boolean permCheck(Member member) {
