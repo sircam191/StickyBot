8a1a3ecff09c7bb20d4a0a8de65e159e93870ae0
Cameron Smith <sircam19@gmail.com>
2020-03-16 12:13:02 -0700

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,33 +1,65 @@
-import net.dv8tion.jda.core.AccountType;
-import net.dv8tion.jda.core.JDA;
-import net.dv8tion.jda.core.JDABuilder;
-import net.dv8tion.jda.core.entities.Game;
+
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
 
 import javax.security.auth.login.LoginException;
+
+
+import java.sql.*;
+import java.util.HashMap;
+import java.util.Map;
+
+import static net.dv8tion.jda.api.entities.Activity.playing;
 
 public class Main {
     public static JDA jda;
     public static String prefix = "?";
 
-    public static String botId = "642587979193516043";
-    public static String token = "SECRET";
+    public static String botId = "******************";
+    public static String token = "*******************************************";
 
-    //TODO Final Bot ID: 628400349979344919
-    //TODO Beta Bot ID: 642587979193516043
+    public static String dbUrl = "jdbc:mysql://***********/stickybot";
+    public static String dbUser = "root";
+    public static String dbPassword = "";
 
-    //TODO Final Build Token: SECRET
-    //TODO Beta Build Token: SECRET
+    public static Map<String, String> mapMessage = new HashMap<>();
+    public static Map<String, String> mapDeleteId = new HashMap<>();
+
+
+    //TODO Final Bot ID: ******************
+    //TODO Beta Bot ID: ******************
+
+    //TODO Final Build Token: *********************************************************
+    //TODO Beta Build Token: *********************************************************
 
     public static void main (String[] args) throws LoginException{
-        jda = new JDABuilder(AccountType.BOT).setToken(token).build();
 
-        jda.getPresence().setGame(Game.playing("?help"));
-       // jda.addEventListener(new StickyCommand());
+        //MySQL DB
+        try {
+            Connection dbConn = DriverManager.getConnection(dbUrl,dbUser,dbPassword);
+            Statement myStmt = dbConn.createStatement();
+            String sql = "select * from messages";
+            ResultSet rs = myStmt.executeQuery(sql);
+
+            while (rs.next()) {
+                mapMessage.put(rs.getString("channelId"),rs.getString("message"));
+                mapDeleteId.put(rs.getString("channelId"),"");
+                System.out.println(rs.getString("channelId") + " -> " + rs.getString("message"));
+            }
+
+        } catch ( SQLException e) {
+            e.printStackTrace();
+        }
+
+        jda = new JDABuilder(token).build();
+
+        jda.getPresence().setActivity(playing("?help"));
+
         jda.addEventListener(new Commands());
         jda.addEventListener(new JoinNewGuild());
-
-        jda.addEventListener(new NewSticky());
-        
+        jda.addEventListener(new Sticky());
         jda.addEventListener(new VirusCommand());
     }
+
 }
+
