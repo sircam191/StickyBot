a7973233c7b4a2c87f59e97a56893456696ec691
Cameron Smith <sircam19@gmail.com>
2020-03-15 19:16:44 -0700

    Update NewSticky.java

--- src/main/java/NewSticky.java
+++ src/main/java/NewSticky.java
@@ -4,14 +4,10 @@ import net.dv8tion.jda.api.entities.*;
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
-import java.util.HashMap;
+import java.sql.*;
 import java.util.List;
-import java.util.Map;
 
 public class Sticky extends ListenerAdapter {
-
-    Map<String, String> mapMessage = new HashMap<>();
-    Map<String, String> mapDeleteId = new HashMap<>();
 
     public void onGuildMessageReceived(GuildMessageReceivedEvent event) {
         String[] args = event.getMessage().getContentRaw().split("\\s+");
@@ -19,7 +15,8 @@ public class Sticky extends ListenerAdapter {
         Guild stickyServer = event.getJDA().getGuildById("641158383138897941");
 
         if (args[0].equalsIgnoreCase(Main.prefix + "stick") && (permCheck(event.getMember() ))) {
-            mapMessage.put(event.getChannel().getId(), "__**Stickied Message:**__\n\n" + event.getMessage().getContentRaw().substring(7));
+            Main.mapMessage.put(event.getChannel().getId(), "__**Stickied Message:**__\n\n" + event.getMessage().getContentRaw().substring(7));
+            addDB(event.getChannel().getId(),event.getMessage().getContentRaw().substring(7));
             event.getMessage().addReaction("\u2705").queue();
 
         } else if (args[0].equalsIgnoreCase(Main.prefix + "stick") && (!permCheck(event.getMember() ))) {
@@ -27,47 +24,40 @@ public class Sticky extends ListenerAdapter {
         }
 
         if ( (args[0].equalsIgnoreCase(Main.prefix + "stickstop") || args[0].equalsIgnoreCase(Main.prefix + "unstick")) && (permCheck(event.getMember() ))) {
-            event.getChannel().deleteMessageById(mapDeleteId.get(event.getChannel().getId())).queue();
+            event.getChannel().deleteMessageById(Main.mapDeleteId.get(event.getChannel().getId())).queue();
+            Main.mapMessage.remove(event.getChannel().getId());
+            Main.mapDeleteId.remove(event.getChannel().getId());
+            removeDB(event.getChannel().getId());
             event.getMessage().addReaction("\u2705").queue();
-        } else  if ( (args[0].equalsIgnoreCase(Main.prefix + "stickstop") || args[0].equalsIgnoreCase(Main.prefix + "unstick")) && (!permCheck(event.getMember() ))) {
+        } else if ( (args[0].equalsIgnoreCase(Main.prefix + "stickstop") || args[0].equalsIgnoreCase(Main.prefix + "unstick")) && (!permCheck(event.getMember() ))) {
             event.getMessage().addReaction("\u274C").queue();
         }
 
-        if ( (args[0].equalsIgnoreCase(Main.prefix + "stickstop") || args[0].equalsIgnoreCase(Main.prefix + "unstick")) && (permCheck(event.getMember() ))) {
-            //adds X emote if user does not have perms to use command
-            if (!permCheck(event.getMember()) || mapMessage.get(event.getChannel().getId()) == null) {
-                event.getMessage().addReaction("\u274C").queue();
-            } else {
-                event.getMessage().addReaction("\u2705").queue();
-            }
-//            if(mapMessage.get(event.getChannel().getId()) != null) {
-//                event.getChannel().deleteMessageById(mapDeleteId.get(event.getChannel().getId())).queue();
-//            }
-            mapMessage.remove(event.getChannel().getId());
-            mapDeleteId.remove(event.getChannel().getId());
-        }
-
-        if (mapMessage.get(event.getChannel().getId()) != null && !event.getAuthor().getId().equals(Main.botId)) {
+        if (Main.mapMessage.get(event.getChannel().getId()) != null && !event.getAuthor().getId().equals(Main.botId)) {
             try {
                 Thread.sleep(50);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-            event.getChannel().sendMessage(mapMessage.get(event.getChannel().getId())).queue(m -> mapDeleteId.put(event.getChannel().getId(), m.getId()));
+            event.getChannel().sendMessage(Main.mapMessage.get(event.getChannel().getId())).queue(m -> Main.mapDeleteId.put(event.getChannel().getId(), m.getId()));
         }
 
 
-        if (mapDeleteId.get(event.getChannel().getId()) != null && (event.getChannel().getLatestMessageId() != mapDeleteId.get(event.getChannel().getId()))) {
+        if (Main.mapDeleteId.get(event.getChannel().getId()) != null && (event.getChannel().getLatestMessageId() != Main.mapDeleteId.get(event.getChannel().getId()))) {
 
             List<Message> messageCheck = event.getChannel().getHistory().retrievePast(1).complete();
 
-           if (!messageCheck.get(0).getContentRaw().contains(mapMessage.get(event.getChannel().getId()))) {
-               event.getChannel().deleteMessageById(mapDeleteId.get(event.getChannel().getId())).queue();
+           if (!messageCheck.get(0).getContentRaw().contains(Main.mapMessage.get(event.getChannel().getId()))) {
+               event.getChannel().deleteMessageById(Main.mapDeleteId.get(event.getChannel().getId())).queue();
            } else {
                List<Message> messageListDelete = event.getChannel().getHistory().retrievePast(10).complete();
                for (Message m : messageListDelete.subList(1, 10)) {
-                   if (m.getContentRaw().contains(mapMessage.get(event.getChannel().getId()))) {
-                       m.delete().queue();
+                   if (m.getContentRaw().contains(Main.mapMessage.get(event.getChannel().getId()))) {
+                       try {
+                           m.delete().queue();
+                       } catch (Exception e) {
+                       }
+
                    }
                }
            }
@@ -79,6 +69,36 @@ public class Sticky extends ListenerAdapter {
             return true;
         } else {
             return false;
+        }
+    }
+
+    public void addDB(String channelId, String message) {
+
+        try {
+            Connection dbConn = DriverManager.getConnection(Main.dbUrl,Main.dbUser,Main.dbPassword);
+            Statement myStmt = dbConn.createStatement();
+            String sql = "INSERT INTO messages (channelId, message)\nVALUES ( '" + channelId + "', '" + message + "' );";
+
+            myStmt.execute(sql);
+            myStmt.close();
+
+        } catch ( SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void removeDB(String channelId) {
+
+        try {
+            Connection dbConn = DriverManager.getConnection(Main.dbUrl,Main.dbUser,Main.dbPassword);
+            Statement myStmt = dbConn.createStatement();
+            String sql = "DELETE FROM messages WHERE channelId='" + channelId + "';";
+
+            myStmt.execute(sql);
+            myStmt.close();
+
+        } catch ( SQLException e) {
+            e.printStackTrace();
         }
     }
 }
