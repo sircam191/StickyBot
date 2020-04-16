c499c1f427909eaa76e81b48a7d4a9dc98713a63
Cameron Smith <sircam19@gmail.com>
2020-04-16 03:18:24 -0700

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,10 @@
 
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
+import org.discordbots.api.client.DiscordBotListAPI;
 
 import javax.security.auth.login.LoginException;
+
 
 import java.sql.*;
 import java.util.HashMap;
@@ -14,26 +16,21 @@ public class Main {
     public static JDA jda;
     public static String prefix = "?";
 
-    public static String botId = "******************";
-    public static String token = "*******************************************";
+    public static String botId = "***************";
+    public static String token = "**************************************";
 
-    public static String dbUrl = "jdbc:mysql://localhost:3306/STICKYBOT4?useUnicode=true&characterEncoding=utf8&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
-    public static String dbUser = "root";
-    public static String dbPassword = "";
+    public static String dbUrl = "jdbc:mysql://localhost:****/STICKYBOT4?useUnicode=true&characterEncoding=utf8&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
+    public static String dbUser = "******";
+    public static String dbPassword = "*******";
+
+    public static String topggAPIToken = "**************************************************************************************************************************";
 
     public static Map<String, String> mapMessage = new HashMap<>();
     public static Map<String, String> mapDeleteId = new HashMap<>();
 
 
-    //TODO Final Bot ID: ******************
-    //TODO Beta Bot ID: ******************
-
-    //TODO Final Build Token: *********************************************************
-    //TODO Beta Build Token: *********************************************************
-
     public static void main (String[] args) throws LoginException{
 
-        //MySQL DB
         try {
             Connection dbConn = DriverManager.getConnection(dbUrl,dbUser,dbPassword);
             Statement myStmt = dbConn.createStatement();
@@ -41,16 +38,20 @@ public class Main {
             ResultSet rs = myStmt.executeQuery(sql);
 
             while (rs.next()) {
-                mapMessage.put(rs.getString("channelId"),rs.getString("message"));
+                mapMessage.put(rs.getString("channelId"),"__**Stickied Message:**__\n\n" + rs.getString("message"));
                 mapDeleteId.put(rs.getString("channelId"),"");
-                System.out.println(rs.getString("channelId") + " -> " + rs.getString("message"));
+                //System.out.println(rs.getString("channelId") + " -> " + rs.getString("message"));
             }
 
         } catch ( SQLException e) {
             e.printStackTrace();
         }
 
-        jda = new JDABuilder(token).build();
+        try {
+            jda = new JDABuilder(token).build().awaitReady();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
 
         jda.getPresence().setActivity(playing("?help"));
 
@@ -58,14 +59,17 @@ public class Main {
         jda.addEventListener(new JoinNewGuild());
         jda.addEventListener(new Sticky());
         jda.addEventListener(new VirusCommand());
-        
-         DiscordBotListAPI api = new DiscordBotListAPI.Builder()
-                .token(token)
-                .botId(botId)
-                .build();
+        jda.addEventListener(new DeleteChannelDBClear());
+
+
+        DiscordBotListAPI api = new DiscordBotListAPI.Builder()
+                .token(topggAPIToken)
+               .botId(botId)
+               .build();
 
         int serverCount = jda.getGuilds().size();
         api.setStats(serverCount);
+
     }
 
 }
