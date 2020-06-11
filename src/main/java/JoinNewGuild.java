b76f2b5056f33372e229dff9d7644e3a024ed703
Cameron Smith <sircam19@gmail.com>
2020-06-10 20:38:59 -0700

    Update JoinNewGuild.java

--- src/main/java/JoinNewGuild.java
+++ src/main/java/JoinNewGuild.java
@@ -7,6 +7,8 @@ import org.discordbots.api.client.DiscordBotListAPI;
 
 import java.awt.*;
 import java.text.NumberFormat;
+import java.time.OffsetDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class JoinNewGuild extends ListenerAdapter {
     @Override
@@ -31,18 +33,22 @@ public class JoinNewGuild extends ListenerAdapter {
         stickySupportServer.getTextChannelById("643974985446326272").sendMessage(em.build()).queue();
 
         //DM server owner info
-        event.getGuild().retrieveOwner().complete().getUser().openPrivateChannel().queue((channel) ->
-        {
-            EmbedBuilder eb = new EmbedBuilder();
-            eb.setColor(Color.yellow);
-            eb.setTitle("__Thank you for adding StickyBot to your server!__");
-            eb.setDescription("Here are the basics to get you started:");
-            eb.addField("Support Server:", "[StickyBot Support](https://discord.gg/SvNQTtf)", false);
-            eb.addField("Vote for StickyBot:", "[top.gg/StickyBot](https://top.gg/bot/628400349979344919)", false);
-            eb.addField("**Commands:** ", "Do ``?commands`` or ``?help``", false);
-            eb.addField("Issues?", "Make sure the bot has perms to send and delete messages and is immune to Slow Mode in the channel.\nJoin the support server with any other issues you may encounter.", false);
-            eb.setFooter("StickyBot", Main.jda.getSelfUser().getAvatarUrl());
-            channel.sendMessage(eb.build()).queue();
+        event.getGuild().retrieveOwner().queue((u) -> {
+            u.getUser().openPrivateChannel().queue((channel) ->
+            {
+                EmbedBuilder eb = new EmbedBuilder();
+                eb.setColor(Color.yellow);
+                eb.setTitle("__Thank you for adding StickyBot to your server!__");
+                eb.setDescription("Here are the basics to get you started:");
+                eb.addField("Support Server:", "[StickyBot Support](https://discord.gg/SvNQTtf)", false);
+                eb.addField("Vote for StickyBot:", "[top.gg/StickyBot](https://top.gg/bot/628400349979344919)", false);
+                eb.addField("**Commands:** ", "Do ``?commands`` or ``?help``", false);
+                eb.addField("Issues?", "Make sure the bot has perms to send and delete messages and is immune to Slow Mode in the channel.\nJoin the support server with any other issues you may encounter.", false);
+                eb.addField("Website:","[www.stickybot.info](https://www.stickybot.info/)", false);
+                eb.addField("Premium: ", "Unlock unlimited stickies and much more. Learn more: [www.stickybot.info/premium](https://www.stickybot.info/premium)", false);
+                eb.setFooter("StickyBot", Main.jda.getShards().get(0).getSelfUser().getAvatarUrl());
+                channel.sendMessage(eb.build()).queue();
+            });
         });
 
         DiscordBotListAPI api = new DiscordBotListAPI.Builder()
@@ -52,5 +58,8 @@ public class JoinNewGuild extends ListenerAdapter {
 
         int serverCount = event.getJDA().getGuilds().size();
         api.setStats(serverCount);
+
     }
+
+
 }
