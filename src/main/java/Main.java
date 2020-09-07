49a05098c4a05cc9a0ef42be5b973403c65dd5b1
Cameron Smith <sircam19@gmail.com>
2020-09-07 16:06:40 -0700

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,19 +1,17 @@
-import net.dv8tion.jda.api.JDA;
-import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.requests.GatewayIntent;
-import net.dv8tion.jda.api.sharding.DefaultShardManager;
 import net.dv8tion.jda.api.sharding.DefaultShardManagerBuilder;
 import net.dv8tion.jda.api.sharding.ShardManager;
 import net.dv8tion.jda.api.utils.ChunkingFilter;
 import net.dv8tion.jda.api.utils.MemberCachePolicy;
 import net.dv8tion.jda.api.utils.cache.CacheFlag;
 import org.discordbots.api.client.DiscordBotListAPI;
+import org.openweathermap.api.DataWeatherClient;
+import org.openweathermap.api.UrlConnectionDataWeatherClient;
 
 import javax.security.auth.login.LoginException;
 
 import java.sql.*;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 import static net.dv8tion.jda.api.entities.Activity.playing;
@@ -23,29 +21,39 @@ public class Main {
     public static String prefix = "?";
 
     public static String botId = "642587979193516043";
-    public static String token = "***";
+    public static String token = "**********";
 
     public static String dbUrl = "jdbc:mysql://localhost:3306/STICKYBOT4?useUnicode=true&characterEncoding=utf8&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
-    public static String dbUser = "***";
-    public static String dbPassword = "***";
+    public static String dbUser = "******************";
+    public static String dbPassword = "*****************";
 
-    public static String topggAPIToken = "***";
+    public static String topggAPIToken = "**********************************";
+    public static DiscordBotListAPI topggAPI;
+
+    public static String weatherAPIToken = "**********************";
+    public static DataWeatherClient weatherHelper;
 
     public static Map<String, String> mapMessage = new HashMap<>();
     public static Map<String, String> mapDeleteId = new HashMap<>();
+
+    public static Map<String, String> mapMessageEmbed = new HashMap<>();
+    public static Map<String, String> mapDeleteIdEmbed = new HashMap<>();
+
     public static Map<String, String> mapPrefix = new HashMap<>();
 
     //subID, prem guildID
     public static Map<String, String> premiumGuilds = new HashMap<>();;
 
+    public static void main (String[] args) throws LoginException {
 
-    //TODO Final Bot ID: 628400349979344919
-    //TODO Beta Bot ID: 642587979193516043
+        try {
+            weatherHelper = new UrlConnectionDataWeatherClient(weatherAPIToken);
+            System.out.println("Weather API Connected");
+        } catch (Exception e) {
+            System.out.println("Weather API Connection Error");
+            e.printStackTrace();
+        }
 
-    //TODO Final Build Token: ***
-    //TODO Beta Build Token: ***
-
-    public static void main (String[] args) throws LoginException{
 
         //Get sticky messages from DB
         try {
@@ -93,6 +101,21 @@ public class Main {
             e.printStackTrace();
         }
 
+        //Get Embeds Stickies from DB
+        try {
+            Connection dbConn = DriverManager.getConnection(dbUrl,dbUser,dbPassword);
+            Statement myStmt = dbConn.createStatement();
+            String sql = "select * from messagesEmbed";
+            ResultSet rs = myStmt.executeQuery(sql);
+
+            while (rs.next()) {
+                mapMessageEmbed.put(rs.getString("channelId"), rs.getString("message"));
+            }
+
+        } catch ( SQLException e) {
+            e.printStackTrace();
+        }
+
         jda = DefaultShardManagerBuilder.create(token,
                 GatewayIntent.GUILD_MESSAGES,
                 GatewayIntent.GUILD_MESSAGE_REACTIONS,
@@ -101,25 +124,28 @@ public class Main {
                  .addEventListeners(
                          new Commands(),
                          new JoinNewGuild(),
-                         new VirusCommand(),
+                         new WeatherCommand(),
                          new DeleteChannelDBClear(),
                          new StickyTime(),
                          new ShardCommands(),
                          new PrefixCommand(),
                          new ManualPremiumCommands(),
-                         new LoadPremiumUpdates()
+                         new LoadPremiumUpdates(),
+                         new StickyEmbed()//,
+                         //new StickyTest()
                  )
                  .disableCache(CacheFlag.ACTIVITY, CacheFlag.VOICE_STATE, CacheFlag.EMOTE, CacheFlag.CLIENT_STATUS)
                  .build();
 
-        jda.setActivity(playing("?help"));
+        jda.setActivity(playing("?help | www.stickybot.info"));
 
-        DiscordBotListAPI api = new DiscordBotListAPI.Builder()
+        topggAPI = new DiscordBotListAPI.Builder()
                .token(topggAPIToken)
                .botId(botId)
                .build();
         int serverCount = (int) jda.getGuildCache().size();
-        api.setStats(serverCount);
+        topggAPI.setStats(serverCount);
+
     }
 }
 
