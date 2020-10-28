4c1eccbe3a4187ce27ef5b5873f0b15757b4a26a
Cameron Smith <sircam19@gmail.com>
2020-10-28 15:33:24 -0700

    Update StickyTime.java

--- src/main/java/StickyTime.java
+++ src/main/java/StickyTime.java
@@ -4,9 +4,11 @@ import net.dv8tion.jda.api.entities.*;
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
+import java.awt.*;
 import java.sql.*;
 import java.text.NumberFormat;
 import java.time.OffsetDateTime;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -16,20 +18,21 @@ public class StickyTime extends ListenerAdapter {
 
         String[] args = event.getMessage().getContentRaw().split("\\s+");
         //Member stickyBot = event.getGuild().getMemberById(Main.botId);
-        Guild stickyServer = event.getJDA().getGuildById("641158383138897941");
         String channelId = event.getChannel().getId();
 
         String prefix = "?";
-        if(Main.mapPrefix.containsKey(event.getGuild().getId())) {
+        if (Main.mapPrefix.containsKey(event.getGuild().getId())) {
             prefix = Main.mapPrefix.get(event.getGuild().getId());
         }
 
         if (args[0].equalsIgnoreCase(prefix + "stick") && (permCheck(event.getMember())) && !event.getAuthor().isBot()) {
             if (guildHasSticky(event.getGuild().getId()) && !Main.premiumGuilds.containsValue(event.getGuild().getId())) {
-                event.getChannel().sendMessage("**There is already an active sticky message in this server in:** " +
-                        event.getGuild().getTextChannelById(getActiveStickyChannelId(event.getGuild().getId())).getAsMention() +
-                        ".\nYou need to stop that sticky using `?stickstop` before creating a new one.\n\n__StickyBot Premium__ allows for unlimited stickies and other features." +
-                        "\nLearn more at https://www.stickybot.info").queue();
+               EmbedBuilder em = new EmbedBuilder();
+                       em.setTitle("**There is already an active sticky message in this server!** ")
+                               .setDescription("Stop the sticky in " + event.getGuild().getTextChannelById(getActiveStickyChannelId(event.getGuild().getId())).getAsMention() + " using `" + prefix + "stickstop` first.")
+                               .addField("__StickyBot Premium__ allows for stickies in as many channels as you like plus other awesome features!", "Try it for free: [www.stickybot.com](https://www.stickybot.info)", false);
+                       event.getChannel().sendMessage(em.setColor(Color.ORANGE).build()).queue();
+
             } else {
                 try {
                     //remove last sticky message if there is one (user used sticky command while already having one)
@@ -40,8 +43,14 @@ public class StickyTime extends ListenerAdapter {
                     //premium sticky
                     if (Main.premiumGuilds.containsValue(event.getGuild().getId())) {
 
-                        String o = event.getMessage().getContentRaw();
-                        String [] arr = o.split(" ", 2);
+                        String input = event.getMessage().getContentRaw();
+
+                        if (input.charAt(input.indexOf(" ") + 1) == '\n') {
+                            throw new Exception("error");
+                        }
+
+
+                        String [] arr = input.split(" ", 2);
                         Main.mapMessage.put(event.getChannel().getId(), arr[1]);
                         removeDB(channelId);
                         addDB(channelId,(arr[1]));
@@ -82,27 +91,20 @@ public class StickyTime extends ListenerAdapter {
         } else if ( (args[0].equalsIgnoreCase(Main.prefix + "stickstop") || args[0].equalsIgnoreCase(Main.prefix + "unstick")) && (!permCheck(event.getMember() ))) {
             //Adds X mark
             event.getMessage().addReaction("\u274C").queue();
-            event.getMessage().addReaction("\u274C").queue();
             event.getChannel().sendMessage("You need the global `Manage Messages` permission to use this command!").queue();
         }
 
-        if(Main.mapMessage.get(channelId) != null) {
-            List<Message> history = event.getChannel().getHistory().retrievePast(5).complete();
-            for(Message m : history) {
+        if (Main.mapMessage.get(channelId) != null) {
+
+            List<Message> history = event.getChannel().getHistory().retrievePast(8).complete();
+
+            for(Message m : history.subList(0, 5)) {
                 //if message is sticky message
                 if(m.getContentRaw().equals(Main.mapMessage.get(channelId))) {
                     //if message is older then 30 sec
                     if(m.getTimeCreated().compareTo(OffsetDateTime.now().minusSeconds(15)) < 0) {
                         m.delete().queue();
                         event.getChannel().sendMessage(Main.mapMessage.get(channelId)).queue(mes -> Main.mapDeleteId.put(channelId, mes.getId()));
-
-                        //Added to make sure it does not bug and send two stickies (next 5 lines)
-                        List<Message> messageListDelete = event.getChannel().getHistory().retrievePast(10).complete();
-                        for (Message mess : messageListDelete.subList(1, 10)) {
-                            if (mess.getContentRaw().contains(Main.mapMessage.get(channelId))) {
-                                mess.delete().queue();
-                            }
-                        }
                     }
                     break;
                 }
@@ -111,7 +113,7 @@ public class StickyTime extends ListenerAdapter {
             //gets set to true if one of last five messages contains sticky message.
             Boolean check = false;
 
-            for(Message m : history) {
+            for(Message m : history.subList(0, 5)) {
                 if(m.getContentRaw().equals(Main.mapMessage.get(channelId))) {
                     check = true;
                 }
@@ -122,6 +124,24 @@ public class StickyTime extends ListenerAdapter {
                 }
                 event.getChannel().sendMessage(Main.mapMessage.get(channelId)).queue();
             }
+
+            //Added to make sure it does not bug and send two stickies (next 5 lines)
+
+            List<Message> indexes = new ArrayList<>();
+
+            for (Message mes : history) {
+                if (mes.getContentRaw().equals(Main.mapMessage.get(channelId))) {
+                    indexes.add(mes);
+                }
+            }
+
+            if (!indexes.isEmpty() && indexes.size() > 1) {
+                indexes.remove(0);
+                for (Message mess : indexes) {
+                    mess.delete().queue();
+                }
+            }
+
         }
     }
 
@@ -137,13 +157,14 @@ public class StickyTime extends ListenerAdapter {
         try {
             Connection dbConn = DriverManager.getConnection(Main.dbUrl,Main.dbUser,Main.dbPassword);
             Statement myStmt = dbConn.createStatement();
-            String sql = "INSERT INTO newMessages (channelId, message)\nVALUES ( '" + channelId + "', '" + message + "' );";
+            String sql = "INSERT INTO newMessages (channelId, message)\nVALUES ( '" + channelId + "', '" + message.replaceAll("'", "") + "' );";
             myStmt.execute(sql);
             myStmt.close();
         } catch ( SQLException e) {
             e.printStackTrace();
         }
     }
+
 
     public void removeDB(String channelId) {
         try {
