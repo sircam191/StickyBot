4085c4d27966b022d9a4455064a827a7c080bd3a
Cameron Smith <sircam19@gmail.com>
2020-11-20 21:24:46 -0800

    Update ShardCommands.java

--- src/main/java/ShardCommands.java
+++ src/main/java/ShardCommands.java
@@ -1,4 +1,5 @@
 import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
@@ -19,9 +20,18 @@ public class ShardCommands extends ListenerAdapter
             emb.setColor(Color.ORANGE);
             emb.setTitle("-Shard Info-");
             emb.addField("Shards: ", "Total Shards: " + (Main.jda.getShardsTotal()) +
-                                                "\nThis Guilds Shard: " + ((event.getGuild().getIdLong() >>> 22) % Main.jda.getShardsTotal() + 1), false);
+                    "\nThis Guilds Shard: " + ((event.getGuild().getIdLong() >>> 22) % Main.jda.getShardsTotal()), false);
+
             event.getChannel().sendMessage(emb.build()).queue();
+
+        } else if (args[0].equalsIgnoreCase(Main.prefix + "shardping")) {
+            String pings = "__**Shard Pings:**__\n";
+            for (JDA shard : Main.jda.getShards()) {
+                pings += "**Shard:** " + shard.getShardInfo().getShardString() + " **Ping:** " + shard.getGatewayPing() + "ms. **Status:** " + shard.getStatus() + "\n";
+            }
+            event.getChannel().sendMessage(pings).queue();
         }
+
     }
 
 }
