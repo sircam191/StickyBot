c60c9a7fb1a5547ae2064317a837d0d80e031786
Cameron Smith <sircam19@gmail.com>
2021-02-10 18:48:16 -0800

    Create AdvancedPoll.java

---
+++ src/main/java/AdvancedPoll.java
@@ -0,0 +1,97 @@
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+
+public class AdvancedPoll extends ListenerAdapter
+{
+    public void onGuildMessageReceived(GuildMessageReceivedEvent event) {
+        String[] args = event.getMessage().getContentRaw().split("\\s+");
+
+        if(event.getAuthor().isBot()) {
+            return;
+        }
+
+        String prefix = "?";
+        if(Main.mapPrefix.containsKey(event.getGuild().getId())) {
+            prefix = Main.mapPrefix.get(event.getGuild().getId());
+        }
+
+
+        if (args[0].equalsIgnoreCase(prefix + "apoll") || args[0].equalsIgnoreCase(prefix + "advancedpoll")) {
+
+
+            try {
+                String rawInput = event.getMessage().getContentRaw().replace(prefix + "apoll", "");
+
+                String[] options = rawInput.split("\\s*,\\s*");
+                EmbedBuilder emb = new EmbedBuilder();
+                emb.setFooter("Poll by: " + event.getAuthor().getAsTag(), event.getAuthor().getAvatarUrl());
+                emb.setColor(event.getGuild().getMemberById(Main.botId).getColor());
+                emb.setDescription(options[0]);
+                emb.setTitle("POLL:");
+
+                if(options.length > 4) {
+
+                    emb.addField( "Options:",
+                            "\uD83C\uDDE6**:** " + options[1] +
+                            "\n\uD83C\uDDE7**:** " + options[2] +
+                            "\n\uD83C\uDDE8**:** " + options[3] +
+                            "\n\uD83C\uDDE9**:** " + options[4]
+                    , false);
+                    event.getChannel().sendMessage(emb.build()).queue(m -> {
+                        m.addReaction("\uD83C\uDDE6").queue();
+                        m.addReaction("\uD83C\uDDE7").queue();
+                        m.addReaction("\uD83C\uDDE8").queue();
+                        m.addReaction("\uD83C\uDDE9").queue();
+                    });
+                    event.getMessage().delete().queue();
+                } else if(options.length > 3) {
+
+                    emb.addField("Options:",
+                            "\uD83C\uDDE6**:** " + options[1] +
+                                    "\n\uD83C\uDDE7**:** " + options[2] +
+                                    "\n\uD83C\uDDE8**:** " + options[3]
+                            , false);
+                    event.getChannel().sendMessage(emb.build()).queue(m -> {
+                        m.addReaction("\uD83C\uDDE6").queue();
+                        m.addReaction("\uD83C\uDDE7").queue();
+                        m.addReaction("\uD83C\uDDE8").queue();
+                    });
+                    event.getMessage().delete().queue();
+                } else if(options.length > 2) {
+                    emb.addField("Options:",
+                            "\uD83C\uDDE6**:** " + options[1] +
+                                    "\n\uD83C\uDDE7**:** " + options[2]
+                            , false);
+                    event.getChannel().sendMessage(emb.build()).queue(m -> {
+                        m.addReaction("\uD83C\uDDE6").queue();
+                        m.addReaction("\uD83C\uDDE7").queue();
+                    });
+                    event.getMessage().delete().queue();
+                } else {
+                    event.getChannel().sendMessage("Please use this format: `" + prefix + "apoll What is your fav color?, Option1, Option2`.\n(*You can have up to 4 options*)").queue();
+                }
+            } catch (Exception e) {
+                event.getChannel().sendMessage("Whoops!\nPlease make sure StickyBot has the `Manage Messages` and `Embed Links` permissions.").queue();
+            }
+
+        }
+
+    }
+
+}
+
+
+/*
+
+?apoll option1, option3
+
+A: \uD83C\uDDE6 (blue)
+B: \uD83C\uDDE7 (blue)
+C: \uD83C\uDDE8 (blue)
+
+ */
