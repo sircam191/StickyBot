490643b861820296d7cac4d246520d3e8da06e9a
Cameron Smith <sircam19@gmail.com>
2021-04-23 11:49:54 -0700

    Update DeleteChannelDBClear.java

--- src/main/java/DeleteChannelDBClear.java
+++ src/main/java/DeleteChannelDBClear.java
@@ -8,18 +8,32 @@ import java.sql.Statement;
 
 public class DeleteChannelDBClear extends ListenerAdapter {
     public void onTextChannelDelete(TextChannelDeleteEvent event) {
-        removeDB(event.getChannel().getId());
-    }
 
-    public void removeDB(String channelId) {
-        try {
-            Connection dbConn = DriverManager.getConnection(Main.dbUrl,Main.dbUser,Main.dbPassword);
-            Statement myStmt = dbConn.createStatement();
-            String sql = "DELETE FROM messages WHERE channelId='" + channelId + "';";
-            myStmt.execute(sql);
-            myStmt.close();
-        } catch ( SQLException e) {
-            e.printStackTrace();
+        if (Main.mapMessage.containsKey(event.getChannel().getId())) {
+            Main.mapMessage.remove(event.getChannel().getId());
+            try {
+                Connection dbConn = DriverManager.getConnection(Main.dbUrl,Main.dbUser,Main.dbPassword);
+                Statement myStmt = dbConn.createStatement();
+                String sql = "DELETE FROM messages WHERE channelId='" + event.getChannel().getId() + "';";
+                myStmt.execute(sql);
+                myStmt.close();
+
+            } catch ( SQLException e) {
+                e.printStackTrace();
+            }
+        
+        } else if (Main.mapMessageEmbed.containsKey(event.getChannel().getId())) {
+            Main.mapMessageEmbed.remove(event.getChannel().getId());
+            try {
+                Connection dbConn = DriverManager.getConnection(Main.dbUrl,Main.dbUser,Main.dbPassword);
+                Statement myStmt = dbConn.createStatement();
+                String sql = "DELETE FROM messagesEmbed WHERE channelId='" + event.getChannel().getId() + "';";
+                myStmt.execute(sql);
+                myStmt.close();
+
+            } catch ( SQLException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
